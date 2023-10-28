dd7e144d165daab9a3fb4b6df7b2231f632b5122
Maxim Marshak <bormaxi1980@gmail.com>
2023-10-28 15:39:30 +0600

    Add starred users list generation

--- builder.py
+++ builder.py
@@ -10,7 +10,8 @@ GITHUB_API_TOKEN = os.environ['GITHUB_API_TOKEN']
 GITHUB_API_HEADER_ACCEPT = "Accept: application/vnd.github+json"
 
 GITHUB_API_URL = "https://api.github.com"
-GITHUB_API_REST = "/user/starred"
+GITHUB_API_STARRED = "/user/starred"
+GITHUB_API_USERS = "/users/"
 
 GITHUB_REPO_URL = "https://github.com/bormaxi8080/github-starred-repos-builder"
 
@@ -19,10 +20,24 @@ HEADERS = {
     "Authorization": "token {0}".format(GITHUB_API_TOKEN)
 }
 
-MD_DOCUMENT_HEADER = "## List of GitHub starred repositories"
+MD_DOCUMENT_HEADER = "## List of GitHub Starred Repositories and Users"
 MD_DOCUMENT_GENERATION = "This document generated automatically, see {0} for details".format(GITHUB_REPO_URL)
 MD_DOCUMENT_LINE_SEPARATOR = "\r\n"
 MD_DOCUMENT_GROUP_SEPARATOR = "----"
+
+
+def fetch_user(user):
+    not_found = "{'message': 'Not Found', 'documentation_url': 'https://docs.github.com/rest/users/users#get-a-user'}"
+    params = None
+
+    res = requests.get(url="{0}{1}{2}".format(GITHUB_API_URL, GITHUB_API_USERS, user),
+                       params=params,
+                       headers=HEADERS)
+    result = res.json()
+    if str(result) == not_found:
+        return None
+    else:
+        return result
 
 
 def fetch_starred_repos(page=""):
@@ -30,7 +45,7 @@ def fetch_starred_repos(page=""):
     if page != '':
         params = {"page": page}
 
-    res = requests.get(url="{0}{1}".format(GITHUB_API_URL, GITHUB_API_REST),
+    res = requests.get(url="{0}{1}".format(GITHUB_API_URL, GITHUB_API_STARRED),
                        params=params,
                        headers=HEADERS)
 
@@ -69,6 +84,9 @@ if __name__ == '__main__':
     STARRED_REPOS = []
     fetched_page = 1
 
+    STARRED_OWNERS_NAMES = []
+    STARRED_OWNERS = []
+
     print("Fetching GitHub starred repos...")
 
     # Fetch first page
@@ -89,22 +107,36 @@ if __name__ == '__main__':
     # Sort array by repository name
     SORTED_REPOS = sorted(STARRED_REPOS, key=lambda x: x['name'])
 
+    print("Generating repos data...")
+
     DOCUMENT_DATE = "**Created at:** {0}".format(datetime.now().date().strftime("%Y-%m-%d"))
 
     # Generate Markdown document
     MD_DOCUMENT = MD_DOCUMENT_HEADER + MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + \
-        MD_DOCUMENT_GENERATION + MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + \
-        "(c) @bormaxi8080, 2023" + MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + \
-        DOCUMENT_DATE + MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + \
-        "**Starred repositories count:** {0}".format(len(STARRED_REPOS)) + \
-        MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + \
-        MD_DOCUMENT_GROUP_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR
+                  MD_DOCUMENT_GENERATION + MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + \
+                  "(c) @bormaxi8080, 2023" + MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + \
+                  DOCUMENT_DATE + MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + \
+                  "**Starred repositories count:** {0}".format(len(STARRED_REPOS)) + \
+                  MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + \
+                  "- [Repositories](#Repositories)" + \
+                  MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + \
+                  "- [Users](#Users)" + \
+                  MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + \
+                  MD_DOCUMENT_GROUP_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR + \
+                  "# Repositories:" + \
+                  MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR
 
     # Save repos to document structure
-    print("Generating Markdown document...")
     for repo in SORTED_REPOS:
         # NOTE: Simplification of the output: image badges have been removed, since with a large number of repositories,
         # a document is generated that is too large and the page freezes.
+
+        # Check repository owner
+        owner = str(repo["owner"]["login"])
+        if owner in STARRED_OWNERS_NAMES:
+            pass
+        else:
+            STARRED_OWNERS_NAMES.append(owner)
 
         # Repository link
         MD_DOCUMENT += "### [{0}]({1}) from [{2}]({3})".format(
@@ -172,15 +204,77 @@ if __name__ == '__main__':
         # Clone
         # MD_DOCUMENT += "[![GitHub clone](https://img.shields.io/badge/GitHub-Clone-green?logo=github)]({0})".format(str(repo["clone_url"]))
         MD_DOCUMENT += "**Clone Url:** {0}".format(str(repo["clone_url"]))
+
         MD_DOCUMENT = _separate(MD_DOCUMENT)
+        MD_DOCUMENT += MD_DOCUMENT_GROUP_SEPARATOR
+        MD_DOCUMENT = _separate(MD_DOCUMENT)
+
+    MD_DOCUMENT += MD_DOCUMENT_GROUP_SEPARATOR
+    MD_DOCUMENT = _separate(MD_DOCUMENT)
+
+    print("Done")
+    print("Generating users data...")
+
+    # Sort owners
+    STARRED_OWNERS_NAMES.sort()
+
+    # Fetch owners data
+    for owner in STARRED_OWNERS_NAMES:
+        owner_data = fetch_user(owner)
+        if owner_data is not None:
+            STARRED_OWNERS.append(owner_data)
+
+    # Generate owners data
+    MD_DOCUMENT += "# Users:" + \
+                   MD_DOCUMENT_LINE_SEPARATOR + MD_DOCUMENT_LINE_SEPARATOR
+
+    for owner in STARRED_OWNERS:
+        MD_DOCUMENT += "### [{0}]({1})".format(str(owner["login"]), str(owner["html_url"]))
+        if owner["name"] is not None:
+            MD_DOCUMENT += " ({0})".format(str(owner["name"]))
+        if owner["location"] is not None:
+            MD_DOCUMENT += ", {0}".format(str(owner["location"]))
+        MD_DOCUMENT = _separate(MD_DOCUMENT)
+
+        if owner["bio"] is not None:
+            MD_DOCUMENT += str(owner["bio"])
+            MD_DOCUMENT = _separate(MD_DOCUMENT)
+        if owner["blog"] is not None:
+            if str(owner["blog"]) != "":
+                MD_DOCUMENT += "Site/Blog: {0}".format(str(owner["blog"]))
+                MD_DOCUMENT = _separate(MD_DOCUMENT)
+
+        MD_DOCUMENT += "Public repos: [{0}]({1}?tab=repositories) / Followers: [{2}]({3})".format(
+            str(owner["public_repos"]),
+            str(owner["html_url"]),
+            str(owner["followers"]),
+            str(owner["followers_url"]))
+        MD_DOCUMENT = _separate(MD_DOCUMENT)
+
+        flag = False
+        if owner["twitter_username"] is not None:
+            flag = True
+            MD_DOCUMENT += "Twitter: [@{0}](https://twitter.com/{1})".format(
+                str(owner["twitter_username"]), str(owner["twitter_username"]))
+            if owner["email"] is not None:
+                MD_DOCUMENT += " / "
+        if owner["email"] is not None:
+            flag = True
+            MD_DOCUMENT += "Email: [{0}](mailto:{1})".format(
+                str(owner["email"]), str(owner["email"]))
+        if flag:
+            MD_DOCUMENT = _separate(MD_DOCUMENT)
 
         MD_DOCUMENT += MD_DOCUMENT_GROUP_SEPARATOR
         MD_DOCUMENT = _separate(MD_DOCUMENT)
+
+    print("Done")
 
     print("Saving document data...")
     # Save Markdown document to file
     _save_document("starred_repos.md", MD_DOCUMENT)
     # Save json backup to file
     _save_json("starred_repos.json", STARRED_REPOS)
+    _save_json("starred_users.json", STARRED_OWNERS)
 
     print("Done")
